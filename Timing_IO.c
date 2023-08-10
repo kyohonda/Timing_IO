38d9bda0679511ae91e5c61a1620249c46bffab6
Kyo-Honda <126185730+kyohonda@users.noreply.github.com>
2023-08-10 18:47:44 +0700

    Create Timing_IO.c

---
+++ Timing_IO.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    double CPU_time_use;
+    clock_t start_time, end_time;
+
+    // non-I/O operation
+    start_time = clock();
+    int result = 0;
+    int i;
+    for (i=0;i<1000000;i++) {
+        result = result+i;
+    }
+    end_time = clock();
+    CPU_time_use = ((double)(end_time-start_time))/CLOCKS_PER_SEC;
+    printf("Non-I/O operation take %f seconds\n", CPU_time_use);
+
+    // I/O operation
+    start_time = clock();
+    for (i=0;i<1000000;i++) {
+        printf("Use I/O operation\n");
+    }
+    end_time = clock();
+    CPU_time_use = ((double)(end_time-start_time))/CLOCKS_PER_SEC;
+    printf("I/O operation take %f seconds\n", CPU_time_use);
+
+    return 0;
+}
