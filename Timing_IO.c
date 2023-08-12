5054226d24349a2a7ef316b5332d571ab12407cf
Kyo-Honda <126185730+kyohonda@users.noreply.github.com>
2023-08-12 22:36:25 +0700

    Update Timing_IO.c

--- Timing_IO.c
+++ Timing_IO.c
@@ -4,26 +4,26 @@
 int main() {
     double CPU_time_use;
     clock_t start_time, end_time;
+    double CPU_time_NonIO, CPU_time_IO;
     int i;
     
     // non-I/O operation
     start_time = clock();
-    int result = 0;
-    for (i=0;i<1000000;i++) {
-        result = result+i;
+    for (i=0;i<1000000000;i++) {
+        int result = i+1;
     }
     end_time = clock();
-    CPU_time_use = ((double)(end_time-start_time))/CLOCKS_PER_SEC;
-    printf("Non-I/O operation take %f seconds\n", CPU_time_use);
+    CPU_time_NonIO = (end_time-start_time)/CLOCKS_PER_SEC;
 
     // I/O operation
     start_time = clock();
-    for (i=0;i<1000000;i++) {
+    for (i=0;i<100000;i++) {
         printf("Use I/O operation\n");
     }
     end_time = clock();
-    CPU_time_use = ((double)(end_time-start_time))/CLOCKS_PER_SEC;
-    printf("I/O operation take %f seconds\n", CPU_time_use);
+    CPU_time_IO = (end_time-start_time)/CLOCKS_PER_SEC;
 
+    printf("Non-I/O operation take %f seconds\n", CPU_time_NonIO);
+    printf("I/O operation take %f seconds\n", CPU_time_IO);
     return 0;
 }
